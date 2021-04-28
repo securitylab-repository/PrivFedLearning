fff343f2702a5a84a76dae85d8ca5a6fc24b399c
SCH <63264140+SC75@users.noreply.github.com>
2021-04-28 17:32:14 +0200

    Create helpers.py

---
+++ helpers.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+class Helpers():
+
+    def to_dataframe(self, input):
+        """
+        Converts input into a dataframe with corresponding output from the
+        first value of the input array
+            Parameters:
+                input (ndarray): features matrix
+            Returns:
+                df (dataframe): dataframe with inputs and outputs
+        """
+        df = pd.DataFrame()
+        df = pd.concat([pd.DataFrame(input)], axis=1)
+        df['Output'] = df[0].map(lambda x: 1 if (x == 1) else 0)
+        df.columns = ['Input_0', 'Input_1', 'Input_2', 'Output']
+        return df
