6156b02b9f060e2b8aa29f9261e93520b3fc77d0
SCH <63264140+SC75@users.noreply.github.com>
2021-05-18 16:36:44 +0200

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -16,3 +16,16 @@ class Helpers():
         df['Output'] = df[0].map(lambda x: 1 if (x == 1) else 0)
         df.columns = ['Input_0', 'Input_1', 'Input_2', 'Output']
         return df
+    
+    def ndarray_to_int(self, ndarray):
+        """
+        Converts ndaray into a list of floats
+            Parameters:
+                ndarray (ndarray): the ndarray you wish to convert
+            Returns:
+                ndarray_int (list): the converted ndarray
+        """
+        ndarray_list = ndarray.tolist()
+        ndarray_str = ''.join(str(e) for e in ndarray_list)
+        ndarray_int = int(''.join(str(ord(c)) for c in ndarray_str[:2]))
+        return ndarray_int
